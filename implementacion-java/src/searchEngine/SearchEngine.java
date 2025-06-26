caf210060aaa1dd1939b1060c30a95ba647a3a02
Fran <155672163+franngo@users.noreply.github.com>
2025-06-26 05:39:26 -0300

    Primera versión del buscador con cantidad de criterios fija y con operadores lógicos todos AND

--- implementacion-java/src/searchEngine/SearchEngine.java
+++ implementacion-java/src/searchEngine/SearchEngine.java
@@ -2,10 +2,16 @@ package searchEngine;
 
 import sample.Sample;
 import java.util.List;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import Enums.*;
+import java.util.stream.*;
 
 public class SearchEngine {
-	public List<Sample> buscar() {
-		
+	public List<Sample> buscar(List<Sample> samples, LocalDate fechaCreacion, LocalDate fechaUltVot, OpinionValue tipoDetectado,
+			String nivelDeVerificacion) {
+		Stream<Sample> ss = samples.stream().filter((s) -> s.getFechaCreacion().isAfter(fechaCreacion) && 
+				s.ultimaVotacion().isAfter(fechaUltVot) && s.currentResult().equals(tipoDetectado) &&
+				s.nivelDeVerificacion().equals(nivelDeVerificacion));
+		return ss.collect(Collectors.toList());
 	}
 }
